dc96d7e2bf298f89007f10bcbba511547cb54cad
Treek <nbatre1@yahoo.com>
2020-01-20 19:39:08 -0600

    keep moving forward

---
+++ client/components/Cart/Cart.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import SubHeader from '../Header/SubHeader';
+import ProductItem from '../Products/ProductItem';
+import Totals from './Totals';
+
+
+const CartWrapper = styled.div`
+
+display:flex;
+justify-content: space-between;
+flex-direction:column;
+margin: 2% 5%;
+`;
+
+const Alert = styled.span`
+  width:100%;
+  text-align:center;
+
+`;
+
+
+const Cart = ({history,loading,error,cart}) => (
+
+    <>
+    {history && (
+        <SubHeader title = 'Cart' goToCart={() => history.push('/cart')} />
+    )}
+
+   {
+    !loading && !error ? (
+        <CartWrapper>
+     <CartItemWrapper>
+         {
+           cart.products &&
+           cart.products.map(product => (
+               <ProductItem key ={product.id} data={product} />
+           ))
+         }
+     </CartItemWrapper>
+     <Totals count = {cart.total} />
+        </CartWrapper>
+
+    ) : (
+        <Alert>
+            {loading ? 'Loading...' : error}
+        </Alert>
+    )
+   }
+    </>
+
+)
+
+
+Cart.defaultProps = {
+
+  loading:false,
+  error:'',
+  cart:{
+    products:[],
+    total:0
+  }
+}
+
+
+export default Cart;
