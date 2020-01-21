79a89b8d85b91fa219ca6eb4b29c2c1be0da3c09
Treek <nbatre1@yahoo.com>
2020-01-20 20:29:34 -0600

    keep moving forward

---
+++ client/components/Cart/CartButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Buttom from '../Button/Button';
+
+
+const CartButton = ({onClick}) => (
+
+
+<Button onClick = {onClick}>
+    {`Cart (0)`}
+</Button>
+
+
+
+)
+
+
+export default CartButton
